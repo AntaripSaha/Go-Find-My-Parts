84d90375d6416dcc8492bb777019caeccaf62124
Antarip Saha <antarip15@gmail.com>
2022-07-20 17:39:51 +0600

    last update 20.07.22

--- resources/views/frontend/new_index.blade.php
+++ resources/views/frontend/new_index.blade.php
@@ -11,7 +11,7 @@
       <meta name="viewport" content="width=device-width,initial-scale=1">
       <meta name="format-detection" content="telephone=no">
       <title>@yield('meta_title', get_setting('website_name').' | '.get_setting('site_motto'))</title>
-      <link rel="icon" type="image/png" href="images/favicon.png">
+      <link rel="icon" type="image/png" href="{{ uploaded_asset(get_setting('site_icon')) }}">
       <!-- fonts -->
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700,700i">
       <!-- css -->
@@ -30,6 +30,8 @@
    <body>
       <!-- site -->
       <div class="site">
+
+
          <!-- site__mobile-header -->
          <header class="site__mobile-header">
             <div class="mobile-header">
@@ -151,16 +153,24 @@
             </div>
          </header>
          <!-- site__mobile-header / end --><!-- site__header -->
+
+
+         
          <header class="site__header">
             <div class="header">
                <div class="header__megamenu-area megamenu-area"></div>
                <div class="header__topbar-start-bg"></div>
                <div class="header__topbar-start">
                   <div class="topbar topbar--spaceship-start">
-                     <div class="topbar__item-text d-none d-xxl-flex">Call Us: (800) 060-0730</div>
-                     <div class="topbar__item-text"><a class="topbar__link" href="about-us.html">About Us</a></div>
+          
+                    @if (get_setting('helpline_number'))
+                     <a href="tel:{{ get_setting('helpline_number') }}" class="topbar__item-text d-none d-xxl-flex">
+                        <div>Call Us: {{ get_setting('helpline_number') }}</div>
+                     </a>
+                     @endif
+                     <!-- <div class="topbar__item-text"><a class="topbar__link" href="about-us.html">About Us</a></div>
                      <div class="topbar__item-text"><a class="topbar__link" href="contact-us-v1.html">Contacts</a></div>
-                     <div class="topbar__item-text"><a class="topbar__link" href="track-order.html">Track Order</a></div>
+                     <div class="topbar__item-text"><a class="topbar__link" href="track-order.html">Track Order</a></div> -->
                   </div>
                </div>
                <div class="header__topbar-end-bg"></div>
@@ -169,7 +179,8 @@
                      <div class="topbar__item-button"><a href="#" class="topbar__button"><span class="topbar__button-label">Compare:</span> <span class="topbar__button-title">5</span></a></div>
                      <div class="topbar__item-button topbar__menu">
                         <button class="topbar__button topbar__button--has-arrow topbar__menu-button" type="button">
-                           <span class="topbar__button-label">Currency:</span> <span class="topbar__button-title">USD</span> 
+                           <span class="topbar__button-label">Currency:</span>
+                            <span class="topbar__button-title">USD</span> 
                            <span class="topbar__button-arrow">
                               <svg width="7px" height="5px">
                                  <path d="M0.280,0.282 C0.645,-0.084 1.238,-0.077 1.596,0.297 L3.504,2.310 L5.413,0.297 C5.770,-0.077 6.363,-0.084 6.728,0.282 C7.080,0.634 7.088,1.203 6.746,1.565 L3.504,5.007 L0.262,1.565 C-0.080,1.203 -0.072,0.634 0.280,0.282 Z"/>
@@ -858,40 +869,24 @@
                   </div>
                </div>
                <div class="header__logo">
-                  <a href="index.html" class="logo">
+                  <a href="{{ route('home') }}" class="logo">
                      <div class="logo__slogan">Auto parts for Cars, trucks and motorcycles</div>
                      <div class="logo__image">
-                        <!-- logo --> 
-                        <svg width="168" height="26">
-                           <path class="logo__part-primary" d="M50,26h-5c-1.1,0-2-0.9-2-2V2c0-1.1,0.9-2,2-2h5c6.6,0,12,5.4,12,12v2C62,20.6,56.6,26,50,26z M57,12
-                              c0-3.9-3.1-7-7-7h-0.8C48.5,5,48,5.5,48,6.2v13.6c0,0.7,0.5,1.2,1.2,1.2H50c3.9,0,7-3.1,7-7V12z M38.5,26h-13h-2
-                              c-0.8,0-1.5-0.7-1.5-1.5v-2v-9v-2v-8v-2C22,0.7,22.7,0,23.5,0h2h13C39.3,0,40,0.7,40,1.5v2C40,4.3,39.3,5,38.5,5H27v5h9.5
-                              c0.8,0,1.5,0.7,1.5,1.5v2c0,0.8-0.7,1.5-1.5,1.5H27v6h11.5c0.8,0,1.5,0.7,1.5,1.5v2C40,25.3,39.3,26,38.5,26z M18.8,23.8
-                              c0.6,1-0.1,2.3-1.3,2.3h-2.3c-0.5,0-1-0.3-1.3-0.8L9.7,18H5v6.5C5,25.3,4.3,26,3.5,26h-2C0.7,26,0,25.3,0,24.5v-23
-                              C0,0.7,0.7,0,1.5,0H10c5,0,9,4,9,9c0,3.2-1.7,6.1-4.3,7.7L18.8,23.8z M10,5H6C5.5,5,5,5.4,5,6v6c0,0.6,0.4,1,1,1h4c2.2,0,4-1.8,4-4
-                              S12.2,5,10,5z"></path>
-                           <path class="logo__part-secondary" d="M166.5,8h-2.3c-0.6,0-1.1-0.4-1.4-1c-0.5-1.2-2-2-3.8-2c-2.2,0-4,1.3-4,3c0,0.9,0.6,1.8,1.5,2.3
-                              c0.2,0.1,0.5,0.3,0.7,0.4c0.9,0.3,1.2,1.3,0.7,2.1l-1,1.7c-0.4,0.7-1.2,0.9-1.9,0.6c-1.2-0.5-2.3-1.3-3.1-2.2c-1.2-1.4-2-3.1-2-5
-                              c0-4.4,4-8,9-8c4.3,0,8,2.6,8.9,6.2C168.2,7.1,167.4,8,166.5,8z M151.5,18h2.3c0.6,0,1.1,0.4,1.4,1c0.5,1.2,2,2,3.8,2
-                              c2.2,0,4-1.3,4-3c0-0.9-0.6-1.8-1.5-2.3c-0.2-0.1-0.5-0.3-0.7-0.4c-0.9-0.3-1.2-1.3-0.7-2.1l1-1.7c0.4-0.6,1.2-0.9,1.9-0.6
-                              c1.2,0.5,2.3,1.3,3.1,2.2c1.2,1.4,2,3.1,2,5c0,4.4-4,8-9,8c-4.3,0-8-2.6-8.9-6.2C149.8,18.9,150.6,18,151.5,18z M146.5,5H140v19.5
-                              c0,0.8-0.7,1.5-1.5,1.5h-2c-0.8,0-1.5-0.7-1.5-1.5V5h-6.5c-0.8,0-1.5-0.7-1.5-1.5v-2c0-0.8,0.7-1.5,1.5-1.5h18
-                              c0.8,0,1.5,0.7,1.5,1.5v2C148,4.3,147.3,5,146.5,5z M125.8,23.8c0.6,1-0.2,2.3-1.3,2.3h-2.3c-0.5,0-1-0.3-1.3-0.8l-4.2-7.3H112v6.5
-                              c0,0.8-0.7,1.5-1.5,1.5h-2c-0.8,0-1.5-0.7-1.5-1.5v-23c0-0.8,0.7-1.5,1.5-1.5h8.5c5,0,9,4,9,9c0,3.2-1.7,6.1-4.3,7.7L125.8,23.8z
-                              M117,5h-4c-0.5,0-1,0.4-1,1v6c0,0.6,0.4,1,1,1h4c2.2,0,4-1.8,4-4S119.2,5,117,5z M103.8,26h-2.3c-0.7,0-1.4-0.4-1.6-1.1l-2.4-6.7
-                              c0-0.1-0.1-0.1-0.2-0.1h-7.5c-0.1,0-0.2,0.1-0.2,0.1l-2.4,6.7c-0.2,0.7-0.9,1.1-1.6,1.1h-2.3c-0.8,0-1.4-0.8-1.1-1.6l8.3-23.3
-                              C90.7,0.4,91.3,0,92,0H95c0.7,0,1.4,0.4,1.6,1.1l8.3,23.3C105.2,25.2,104.6,26,103.8,26z M95.5,12.7l-1.8-4.9
-                              c-0.1-0.2-0.3-0.2-0.4,0l-1.8,4.9c0,0.1,0.1,0.3,0.2,0.3h3.5C95.4,13,95.5,12.9,95.5,12.7z M83.9,10.2c0,0.2-0.1,0.4-0.1,0.6
-                              c0,0.2-0.1,0.4-0.1,0.6c-0.1,0.5-0.3,1.1-0.6,1.6c-0.1,0.1-0.1,0.3-0.2,0.4c-0.1,0.1-0.1,0.2-0.2,0.4c-0.2,0.4-0.5,0.7-0.8,1.1
-                              c-0.1,0.1-0.2,0.2-0.3,0.3c-0.1,0.1-0.2,0.2-0.3,0.3c-0.5,0.5-1.1,0.9-1.7,1.3c-1.4,0.8-3,1.3-4.7,1.3h-5v6.5c0,0.8-0.7,1.5-1.5,1.5
-                              h-2c-0.8,0-1.5-0.7-1.5-1.5v-23C65,0.7,65.7,0,66.5,0H75c1.7,0,3.3,0.5,4.7,1.3c0.6,0.4,1.2,0.8,1.7,1.3c0.1,0.1,0.2,0.2,0.3,0.3
-                              c0.1,0.1,0.2,0.2,0.3,0.3c0.3,0.3,0.5,0.7,0.8,1.1c0.1,0.1,0.1,0.2,0.2,0.3C83,4.8,83.1,5,83.1,5.1c0.2,0.5,0.4,1,0.6,1.6
-                              c0,0.2,0.1,0.4,0.1,0.6c0,0.2,0.1,0.4,0.1,0.6C83.9,8,84,8.2,84,8.4c0,0.2,0,0.4,0,0.6s0,0.4,0,0.6C84,9.8,83.9,10,83.9,10.2z M75,5
-                              h-4c-0.6,0-1,0.4-1,1v6c0,0.6,0.4,1,1,1h4c2.2,0,4-1.8,4-4S77.2,5,75,5z"></path>
-                        </svg>
+                        <!-- logo -->          
+                           @php
+                              $header_logo = get_setting('header_logo');
+                           @endphp
+                           @if($header_logo != null)
+                              <img src="{{ uploaded_asset($header_logo) }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
+                           @else
+                              <img src="{{ static_asset('assets/img/logo.png') }}" alt="{{ env('APP_NAME') }}" class="mw-100 h-30px h-md-40px" height="40">
+                           @endif
+                   
                         <!-- logo / end -->
                      </div>
+                     
                   </a>
+
                </div>
                <div class="header__search">
                   <div class="search">
@@ -3088,9 +3083,9 @@
                   <div class="container">
                      <div class="site-footer__bottom-row">
                         <div class="site-footer__copyright">
-                           <!-- copyright --> Powered by HTML — Designed by <a href="https://themeforest.net/user/kos9" target="_blank">Kos</a><!-- copyright / end -->
+                           <!-- copyright --> Copyright 2022 -<a href="http://www.gofindmyparts.com/" target="_blank" style="color: green;">Go Find My Parts.</a> All rights reversed<!-- copyright / end -->
                         </div>
-                        <div class="site-footer__payments"><img src="images/payments.png" alt=""></div>
+                        <div class="site-footer__payments"><img src="{{static_asset('assets/frontend/images/payments.png')}}" alt=""></div>
                      </div>
                   </div>
                </div>
@@ -3098,6 +3093,18 @@
          </footer>
          <!-- site__footer / end -->
       </div>
+
+
+
+
+
+
+
+
+
+
+
+
       <!-- site / end --><!-- mobile-menu -->
       <div class="mobile-menu">
          <div class="mobile-menu__backdrop"></div>
@@ -3895,6 +3902,10 @@
             </div>
          </div>
       </div>
+
+
+
+
       <!-- photoswipe / end --><!-- scripts -->
       <script src="{{static_asset('assets/frontend/vendor/jquery/jquery.min.js')}}"></script>
       <script src="{{static_asset('assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
