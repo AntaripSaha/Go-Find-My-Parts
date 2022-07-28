026a422d5376e79d4e8547d48ae6b65ef8ccce13
Antarip Saha <antarip15@gmail.com>
2022-07-28 17:21:17 +0600

    Working on frontend

--- resources/views/frontend/inc/nav.blade.php
+++ resources/views/frontend/inc/nav.blade.php
@@ -201,11 +201,23 @@
                                 <div class="d-lg-none" data-toggle="class-toggle" data-target=".front-header-search">
                                     <button class="btn px-2" type="button"><i class="la la-2x la-long-arrow-left"></i></button>
                                 </div>
+
+                                {{-- <div class="form-group">
+                                   
+                                    <input type="text" style="border-radius: 20px" class="form-control is-invalid" id="inputSuccess3">
+                                    <div class="valid-feedback feedback-icon">
+                                        <i class="fa fa-check"></i>
+                                    </div>
+                                    <div class="invalid-feedback feedback-icon" style="background-color: red; width: 30px; height: 30px; border-radius: 20px">
+                                        <i class="fa fa-search"></i>
+                                    </div>
+                                </div> --}}
+
                                 <div class="input-group" >
                                     <input type="text" style="border-radius: 20px !important" class="border-0 border-lg form-control" id="search" name="keyword" @isset($query)
                                         value="{{ $query }}"
                                     @endisset placeholder="{{translate('I am shopping for...')}}" autocomplete="off">
-                                    <div class="  d-none d-lg-block">
+                                    <div class=" input-group-append d-none d-lg-block">
                                         <button class="btn btn-primary" style="border-radius:50% !important; background: #72b860 !important; border-color: #72b860;" type="submit">
                                             <i class="la la-search"></i>
                                         </button>
--- resources/views/frontend/partials/wishlist.blade.php
+++ resources/views/frontend/partials/wishlist.blade.php
@@ -1,11 +1,23 @@
+<style>
+    .heart{
+        background-color:rgb(112, 206, 112) !important;
+        margin-bottom: 26px;
+        margin-right: 4px;
+        padding: 7px;
+    }
+    .icon{
+        color: #f5f502 !important;
+        font-size: 35px !important;
+    }
+</style>
 <a href="{{ route('wishlists.index') }}" class="d-flex align-items-center text-reset">
-    <i class="la la-heart-o la-2x opacity-80"></i>
+    <i class="la la-heart-o la-3x opacity-90 icon"></i>
     <span class="flex-grow-1 ml-1">
         @if(Auth::check())
-            <span class="badge badge-primary badge-inline badge-pill">{{ count(Auth::user()->wishlists)}}</span>
+            <span class="badge badge-primary badge-inline badge-pill heart">{{ count(Auth::user()->wishlists)}}</span>
         @else
-            <span class="badge badge-primary badge-inline badge-pill">0</span>
+            <span class="badge badge-primary badge-inline badge-pill heart" style="">0</span>
         @endif
-        <span class="nav-box-text d-none d-xl-block opacity-70">{{translate('Wishlist')}}</span>
+        {{-- <span class="nav-box-text d-none d-xl-block opacity-70">{{translate('Wishlist')}}</span> --}}
     </span>
 </a>
