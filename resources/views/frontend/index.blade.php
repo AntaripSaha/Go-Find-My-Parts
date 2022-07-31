727c89cd498c034dfc40361b47793fa79c56d220
Antarip Saha <antarip15@gmail.com>
2022-07-31 17:27:26 +0600

    advertise templating done

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -189,54 +189,11 @@
                </div>
             </div>
 
-            
             {{-- //owl-carousel --}}
-            <div class="block-banners block">
-               <div class="container">
-                  <div class="block-banners__list">
-                     <div class="adone owl-carousel owl-theme">
-     
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner1.jpg')}}" alt="" width="auto" height="350px">
-                              </span>
-                           </a>
-                        </div>
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner2.jpg')}}" alt=""  width="auto" height="350px">
-                              </span>
-                           </a>
-                        </div>
-
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner3.jpg')}}" alt=""  width="auto" height="350px">
-                              </span>
-                           </a>
-                        </div>
-
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner4.jpg')}}" alt=""  width="auto" height="350px">
-                              </span>
-                           </a>
-                        </div>
-                        
-                    </div>
-
-
-                  </div>
-               </div>
-            </div>
-
-
- 
+            <div class="block-space block-space--layout--divider-nl"></div>
+            @include('frontend.redparts.advertise_two')
             {{-- //owl-carousel --}}
+
 
             <div class="block-space block-space--layout--divider-nl"></div>
 
@@ -253,67 +210,10 @@
             <!-------------- Product Section End------------->
 
 
-
-            <div class="block-space block-space--layout--divider-nl"></div>
- 
-
-            
             {{-- //owl-carousel --}}
-            <div class="block-banners block">
-               <div class="container">
-                  <div class="block-banners__list">
-                     <div class="ad owl-carousel owl-theme">
-     
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner1.jpg')}}" alt=""  width="auto" height="250px">
-                              </span>
-                           </a>
-                        </div>
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner2.jpg')}}" alt=""  width="auto" height="250px">
-                              </span>
-                           </a>
-                        </div>
-
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner3.jpg')}}" alt=""  width="auto" height="250px">
-                              </span>
-                           </a>
-                        </div>
-
-                        <div class="item">
-                           <a href="#" class=" ">
-                              <span class="">
-                                 <img src="{{static_asset('assets/frontend/images/banners/banner4.jpg')}}" alt=""  width="auto" height="250px">
-                              </span>
-                           </a>
-                        </div>
-                        
-                    </div>
-
-
-                  </div>
-               </div>
-            </div>
-
-
- 
-            {{-- //owl-carousel --}}
-
             <div class="block-space block-space--layout--divider-nl"></div>
-
-
-
-
-
-
-            
+            @include('frontend.redparts.advertise_one')
+            {{-- //owl-carousel --}}
             <div class="block-space block-space--layout--divider-nl"></div>
 
                @include('frontend.redparts.new_arrival')
